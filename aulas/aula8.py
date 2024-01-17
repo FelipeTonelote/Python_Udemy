748dc7536c927e2815f368fb04357ddd53634064
Felipe <felipe.tonelote@hotmail.com>
2024-01-17 13:50:27 -0300

    novo repositorio aula udemy

---
+++ aulas/aula8.py
@@ -0,0 +1,13 @@
+nome = 'Felipe'
+sobrenome = 'Tonelote'
+idade = 19
+ano_nascimento = 2024 - idade
+maior_idade = idade >= 18
+altura_metros = 1.82
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano nascimento:', ano_nascimento)
+print('É maior?', maior_idade)
+print('Altura em metros:', altura_metros)
