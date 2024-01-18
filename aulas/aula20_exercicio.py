159b2e28d45c228023d5e4f8f46a92637d67134a
Felipe <felipe.tonelote@hotmail.com>
2024-01-17 22:04:32 -0300

    aulas 19 a 24

---
+++ aulas/aula20_exercicio.py
@@ -0,0 +1,7 @@
+valor1 = input('Digite um valor: ')
+valor2 = input('Digite outro valor: ')
+
+if valor1 >= valor2:
+    print(f'{valor1=} é maior ou igual que {valor2=}.')
+else:
+    print(f'{valor2=} é maior do que {valor1=}.')
